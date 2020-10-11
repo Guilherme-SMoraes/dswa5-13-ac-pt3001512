01139ad0ef1eb925b354d912a0a17108da4669cc
Guilherme-SMoraes <guisanmo1@hotmail.com>
2020-10-11 19:45:47 -0300

    Adicionado mongoDB

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,54 +1,60 @@
 var ID_CONTATO_INC = 3;
 
-var contatos = [
-    { _id: 1, nome: 'Fabio Teixeira', email: 'fabio.teixeira@ifsp.edu.br' },
-    { _id: 2, nome: 'Fabiano Teixeira', email: 'fabiano.teixeira@ifsp.edu.br' },
-    { _id: 3, nome: 'Melissa Teixeira', email: 'melissa.teixeira@ifsp.edu.br' }
-]
-
-module.exports = function() {
+module.exports = function (app) {
+    var Contato = app.models.contato;
     var controller = {};
-    controller.listaContatos = function(req, res) {
-        res.json(contatos);
+    controller.listaContatos = function (req, res) {
+        Contato.find().exec().then(function (contatos) {
+                res.json(contatos);
+            },
+            function (erro) {
+                console.log(erro);
+                res.status(500).json(erro);
+            });
     };
-    controller.obtemContato = function(req, res) {
-        console.log('Selecionou o contato: ' + req.params.id);
-        var idContato = req.params.id;
-        var contato = contatos.filter(function(contato) {
-            return contato._id == idContato;
-        })[0];
-        contato ? res.json(contato) : res.status(404).send('Contato não encontrado!');
+    controller.obtemContato = function (req, res) {
+        var _id = req.params.id;
+        Contato.findById(_id).exec().then(function (contato) {
+                if (!contato) throw new Error("Contato não encontrado");
+                res.json(contato);
+            },
+            function (error) {
+                console.log(error);
+                res.status(404).json(error);
+            });
     };
-    controller.removeContato = function(req, res) {
-        var idContato = req.params.id;
-        contatos = contatos.filter(function(contato) {
-            return contato._id != idContato;
-        });
-        res.send(204).end();
+    controller.removeContato = function (req, res) {
+        var _id = req.params.id;
+        Contato.deleteOne({
+            "_id": _id
+        }).exec().then(function () {
+                res.end();
+            },
+            function (error) {
+                return console.error(error);
+            });
     };
 
-    controller.salvaContato = function(req, res) {
-        var contato = req.body;
-        contato = contato._id ? atualiza(contato) : adiciona(contato);
-        res.json(contato);
-    };
-
-    function adiciona(contatoNovo) {
-        contatoNovo._id = ++ID_CONTATO_INC;;
-        contatos.push(contatoNovo);
-        return contatoNovo;
-    }
-
-    function atualiza(contatoAlterar) {
-        contatos = contatos.map(function(contato) {
-        if(contato._id == contatoAlterar._id) {
-            contato = contatoAlterar;
+    controller.salvaContato = function (req, res) {
+        var _id = req.params.id;
+        if (_id) {
+            Contato.findByIdAndUpdate(_id, res.body).exec().then(function (contato) {
+                    res.json(contato);
+                },
+                function (error) {
+                    console.log(erro);
+                    res.status(500).json(erro);
+                });
+        } else {
+            Contato.create(req.body).then(function (contato) {
+                res.status(201).json(contato);
+            }, 
+            function (error) {
+                console.log(erro);
+                res.status(500).json(erro);
+            });
         }
-        return contato;
-    });
-
-    return contatoAlterar;
-  }
+    };
 
     return controller;
 };
